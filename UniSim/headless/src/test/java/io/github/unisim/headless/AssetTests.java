9a4b7d12d05048f8eae2e745793f8a1c58dadb5d
wsx501 <wsx501@york.ac.uk>
2024-11-25 14:02:51 +0000

    Updated test to check if Library building asset exists

--- UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
+++ UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
@@ -1,16 +1,14 @@
 package io.github.unisim.headless;
 
 import com.badlogic.gdx.Gdx;
-import io.github.unisim.ui.BuildingMenu;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssetTests {
     @Test
-    public void testCanteenAssetExists() {
+    public void testLibraryExists() {
         HeadlessLauncher.main(new String[0]);
-        assertTrue(Gdx.files.internal(BuildingMenu.buildings.get(0).texture).exists(),
-            "The asset for the canteen building should be available");
+        assertTrue(Gdx.files.internal("library.png").exists(), "The asset for the Library Building should be available");
     }
 }
