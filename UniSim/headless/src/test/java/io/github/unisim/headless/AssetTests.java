5eed5b2d2b6b5275ee67288dcadfff15127f31a7
vxs506 <vxs506@york.ac.uk>
2024-11-26 20:32:11 +0000

    added check for assets.txt

--- UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
+++ UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
@@ -12,13 +12,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssetTests {
 
-    /*
+    /**
     Checks required files and their paths within assets folder from "assets.txt" in assets folder
     Tests that all named files are present
-     */
+     **/
     @Test
     public void AssetTestMain() throws IOException {
         HeadlessLauncher.main(new String[0]);
+        assertTrue(Gdx.files.internal("assets.txt").exists(), "Assets.txt should be available");
         File file = new File("../assets/assets.txt");
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
