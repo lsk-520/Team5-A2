3768799652b667ae607c7cf3f2f3caf4be4651a9
vxs506 <vxs506@york.ac.uk>
2024-11-26 15:08:32 +0000

    Added test for presence of each file in assets

--- UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
+++ UniSim/headless/src/test/java/io/github/unisim/headless/AssetTests.java
@@ -3,12 +3,27 @@ package io.github.unisim.headless;
 import com.badlogic.gdx.Gdx;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssetTests {
+
+    /*
+    Checks required files and their paths within assets folder from "assets.txt" in assets folder
+    Tests that all named files are present
+     */
     @Test
-    public void testLibraryExists() {
+    public void AssetTestMain() throws IOException {
         HeadlessLauncher.main(new String[0]);
-        assertTrue(Gdx.files.internal("buildings/library.png").exists(), "The asset for the Library Building should be available");
+        File file = new File("../assets/assets.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            assertTrue(Gdx.files.internal(line).exists(), "Asset '" + line + "'  should be available");
+        }
     }
 }
